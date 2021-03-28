32149b722bf62c4a5033cbd599a1e440c21775b8
necip <ncpyolcu@gmail.com>
2021-03-28 15:09:26 +0300

    Maps and listview are added

---
+++ app/src/main/java/com/example/parkyeriharita/MapsActivity.java
@@ -0,0 +1,74 @@
+package com.example.parkyeriharita;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    ListView listView;
+    TextView tx1, tx2, tx3, tx4;
+    ArrayAdapter<String> adapter;
+    Context context = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        listView = findViewById(R.id.listView);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(context, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        adapter = new ArrayAdapter<>(this, R.layout.row_layout, R.id.tx1, new String[]{"park 1", "park 2",
+                "park 3", "park 4", "park 5", "park 6", "park 7", "park 8", "park 9", "park 10", "park 11"});
+        listView.setAdapter(adapter);
+
+
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(41, 29);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("İstanbul"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
